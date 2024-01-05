36cd2949ebe6b119370bd8bdee358d20b520b9cc
Diego AVZ <124291993+Diego-AVZ@users.noreply.github.com>
2024-01-05 12:15:33 +0100

    Update js1.js

--- js1.js
+++ js1.js
@@ -50,3 +50,60 @@ mp2.addEventListener("click", function () {
     "AlphaBase is a state-of-the-art social platform revolutionizing cryptocurrency insights. It establishes a decentralized bond between AlphaProvs and AlphaScouts, enabling the seamless sharing of privileged information. Through the innovative SignalAscend algorithm, signals are strategically positioned in the global list based on the AlphaScore, creating a dynamic and fair hierarchy. AlphaBase not only redefines the crypto landscape but also introduces a novel earning model, making it a collaborative ecosystem where users contribute, earn, and thrive.";
   document.getElementById("pdesc").style.fontSize = "1.3vw";
 });
+
+var but1 = document.getElementById("but1");
+var ani1 = document.getElementById("ani1");
+
+but1.addEventListener("mouseover", function(){
+  ani1.style.width = "500px";
+  ani1.style.height = "500px";
+  document.getElementById("but1").style.color = "black";
+  document.getElementById("but1").style.textShadow = "none";
+})
+
+but1.addEventListener("mouseout", function () {
+  ani1.style.width = "1px";
+  ani1.style.height = "1px";
+  document.getElementById("but1").style.color = "white";
+  document.getElementById("but1").style.textShadow =
+    "0px 0px 10px rgb(0, 0, 0)";
+
+});
+
+
+//____________________________________________________________
+//BOTs TELEGRAM
+
+var mes = document.getElementById("mesIn");
+
+function sendToTelegram() {
+
+    const botToken = "6793857811:AAGwtXd5rWJddn_LamSZqBOwdFPMDgAKgcc";
+
+    const telegramApiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+    var message = mes.value;
+
+    const messageData = {
+      chat_id: -4084847055,
+      text: message,
+    };
+
+    console.log("sending to Telegram. . . ");
+
+    fetch(telegramApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messageData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Mensaje enviado con éxito:", data);
+      })
+      .catch((error) => {
+        console.error("Error al enviar el mensaje:", error);
+      });
+
+}
